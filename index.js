9b8b2afa52721315e6b1e947568c098ad7e83dc9
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-13 22:00:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function breakOut(array, changeValue, stopValue) {
+  let i = 0;
+  while (i < array.length -1) {
+    if (array[i] === stopValue) {
+      break;
+    } else {
+      array[i] = changeValue;
+      i++;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  let i = 0;
+  while (i < array.length -1) {
+    if (array[i] === skipValue) {
+      continue;
+    } else {
+      array[i] = changeValue;
+    }
+    i++
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  var x = findFn;
+  while (i < array.length -1) {
+    if (array[i] === x) {
+      return array[i];
+    } else {
+      return null;
+    }
+  }
+}
